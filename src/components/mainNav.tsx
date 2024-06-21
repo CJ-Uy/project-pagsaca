675bedc7e90abfde1f8a889bd330307dc3afc101
Charles Joshua Uy <129761131+CJ-Uy-EVC@users.noreply.github.com>
2024-06-21 14:21:52 +0800

    Added Base Dashboard Template

--- src/components/mainNav.tsx
+++ src/components/mainNav.tsx
@@ -1,19 +1,28 @@
+"use client";
+
 import Link from "next/link"; // Import the Link component
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 const mainNavItems = [
   { label: "Home", path: "/" },
   { label: "About", path: "/about" },
   { label: "Dashboard", path: "/dashboard" },
-  { label: "My Plant Cells", path: "/plant-cells"}
+  { label: "My Plant Cells", path: "/plant-cells" },
 ];
 
 export default function MainNav() {
+  const [ selectedItem, setSelectedItem ] = useState("/");
   return (
     <div className="mr-4 hidden gap-2 md:flex">
       {mainNavItems.map((item, index) => (
         <Link key={index} href={item.path}>
-          <Button key={index} variant="link">
+          <Button
+            key={index}
+            variant="link"
+            className={selectedItem === item.path ? "text-green-500" : ""}
+            onClick={() => setSelectedItem(item.path)}
+          >
             {item.label}
           </Button>
         </Link>
