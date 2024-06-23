8a5365c88e525fc19c5aae06fe7943e8abe346ec
Charles Joshua Uy <129761131+CJ-Uy-EVC@users.noreply.github.com>
2024-06-23 08:07:19 +0800

    moved from drizzle to prisma

--- supabase/migrations/0001_mushy_hammerhead.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "voltage_table" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp NOT NULL,
-	"voltage" double precision NOT NULL
-);
---> statement-breakpoint
-DROP TABLE "posts_table";--> statement-breakpoint
-DROP TABLE "users_table";
